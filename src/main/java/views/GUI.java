6d5b0f2438fe2504a8600495b635e54d75317423
Jacob Burenstam <burenstam@gmail.com>
2014-01-24 19:49:44 +0100

    Added GUI. Added dependency and plugin to pom.xml

---
+++ src/main/java/views/GUI.java
@@ -0,0 +1,33 @@
+package views;
+
+import utils.Enduro;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+    private static final long serialVersionUID = -2948560310654842046L;
+    private JPanel buttonPanel;
+    private JTextArea textArea;
+    private JButton button;
+
+
+    public GUI(){
+        this.setTitle(Enduro.APPLCATION_NAME);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(true);
+        this.setLayout(new BorderLayout());
+        this.setSize(java.awt.Toolkit.getDefaultToolkit().getScreenSize().width/2,java.awt.Toolkit.getDefaultToolkit().getScreenSize().height/2);
+        this.setLocationRelativeTo(null);
+
+        buttonPanel = new JPanel();
+        button =new JButton("Example");
+        buttonPanel.add(button);
+        this.add(buttonPanel, BorderLayout.EAST);
+
+        textArea = new JTextArea("Hello World");
+        this.add(textArea, BorderLayout.CENTER);
+    }
+
+}
