22429c387b781feded9bc74f717be36691c4166c
Per-Victor Persson <dracr@gmail.com>
2014-01-27 13:42:53 +0100

    update4

--- src/main/java/views/GUI.java
+++ src/main/java/views/GUI.java
@@ -3,13 +3,15 @@ package views;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class GUI extends JFrame {
 
 	private static final long serialVersionUID = -2948560310654842046L;
 
 	private JPanel topPanel;
-	private JTextArea textArea;
+	private JTextArea registerArea;
 	private JButton registerButton;
 
 	public GUI() {
@@ -28,12 +30,46 @@ public class GUI extends JFrame {
 		topPanel.setLayout(new GridLayout(1, 2));
 		registerButton = new JButton("Register");
 		registerButton.setPreferredSize(new Dimension(width / 2, height / 2));
+		registerButton.addMouseListener(new ClickListener());
 		topPanel.add(new TextArea());
 		topPanel.add(registerButton);
 
-		textArea = new JTextArea("Hello World");
-		textArea.setEditable(false);
-		this.add(textArea);
+		registerArea = new JTextArea("Hello World");
+		registerArea.setEditable(false);
+		this.add(registerArea);
+	}
+	
+	private class ClickListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 
 }
