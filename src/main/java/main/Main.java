fb7d9c627746dba9ffe6c47e41411bbfa9ebf206
Linus Lexfors <bas11lle@alfa-3.student.lth.se>
2014-01-27 14:23:55 +0100

    Cleanup main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,6 @@
 package main;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Iterator;
 
-import models.Participant;
-import utils.Enduro;
-import utils.FileReader;
-import utils.FileWriter;
 import views.GUI;
 
 public class Main {
