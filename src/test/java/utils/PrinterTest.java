f4b882b7d98dbd7f6f7e18b8d23d83b5145f58d7
Linus Lexfors <bas11lle@alfa-5.student.lth.se>
2014-02-03 09:52:54 +0100

    finished tast 4.2, changed printer to formatter

--- src/test/java/utils/PrinterTest.java
+++ src/test/java/utils/PrinterTest.java
@@ -1,12 +1,10 @@
 package utils;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Iterator;
-
-import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
@@ -16,13 +14,15 @@ public class PrinterTest {
 
 	private ArrayList<String> startTimes;
 	private ArrayList<String> finishTimes;
-	private Printer printer;
+	private Formatter formatter;
+	private Enduro enduro;
 
 	@Before
 	public void setUp() throws Exception {
-		printer = new Printer();
+		formatter = new Formatter();
 		startTimes = new ArrayList<String>();
 		finishTimes = new ArrayList<String>();
+		enduro = Enduro.getInstance();
 	}
 
 	@After
@@ -37,31 +37,25 @@ public class PrinterTest {
 		finishTimes.add("13.13.13");
 		finishTimes.add("19.19.19");
 		finishTimes.add("20.16.48");
-
-		String resultList = printer.generateResultList(startTimes, finishTimes);
-
+		String resultList = formatter.generateResultList(startTimes, finishTimes);
 		assertEquals(resultList, "StartNo; TotalTime; StartTime; ResultTime\n"
 				+ "1; --.--.--; 12.12.12; 13.13.13\n"
 				+ "2; --.--.--; 14.14.14; 19.19.19\n"
 				+ "3; --.--.--; 17.17.17; 20.16.48\n");
-
 	}
 
 	@Test
 	public void testEmptyLists() {
-		Printer printer = new Printer();
+		Formatter printer = new Formatter();
 		String result = printer.generateResultList(startTimes, finishTimes);
 		assertEquals(result, "Both lists are empty!");
-
 	}
 
 	@Test
 	public void testResultsWithFiles() throws FileNotFoundException {
-
 		FileReader f = new FileReader();
 		String path = Enduro.getInstance().getResourcePath(
 				"acceptanstester/iteration1/acceptanstest3/resultat.txt");
-
 		Iterator<String> iter;
 		StringBuilder sb = new StringBuilder();
 		try {
@@ -69,29 +63,70 @@ public class PrinterTest {
 			while (iter.hasNext()) {
 				sb.append(iter.next() + "\n");
 			}
-
 		} catch (FileNotFoundException e) {
 			
 			e.printStackTrace();
 		}
 		assertEquals(
-				printer.generateResultList("starttider.txt", "maltider.txt"),
+				formatter.generateResultList("starttider.txt", "maltider.txt"),
 				sb.toString());
-		
-		System.out.print(sb.toString());
 	}
 
 	@Test
 	public void testWrongFilePath() {
 		boolean success = false;
 		try {
-			printer.generateResultList("lololzosdldsl",
+			formatter.generateResultList("lololzosdldsl",
 					"dhrlhrol.malware.exe.virus.ru.warez");
 		} catch (FileNotFoundException e) {
 			success = true;
 		} finally {
-			Assert.assertEquals("Should have raised FileNotFoundException",
+			assertEquals("Should have raised FileNotFoundException",
 					true, success);
 		}
+	}
+	
+	@Test
+	public void testReadColumnNames() {
+		try {
+			String path = Enduro.getInstance().getResourcePath(
+					"acceptanstester/iteration1/acceptanstest3_4/");
+			
+			
+			assertEquals(formatter.readColumnNames(path + "namnfil.txt"), "StartNo; Namn");
+			
+			
+
+			assertEquals(formatter.readColumnNames(path + "resultat.txt"), "StartNo; Name; TotalTime; StartTime; ResultTime");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	@Test
+	public void testReadColumnNamesFileNotFoundException() {
+		boolean success = false;
+		try {
+			formatter.readColumnNames(Enduro.getInstance().getResourcePath(
+				"acceptanstester/iteration1/acceptanstest3_4/catsNdogs.txt"));
+			
+	
+		} catch (FileNotFoundException e) {
+			success = true;
+		} finally {
+			assertEquals("Should have raised FileNotFoundException",
+					true, success);
+		}
+	}
+	
+	@Test
+	public void testEmptyFileException() throws FileNotFoundException {
+		boolean success = false;
+		try {
+			formatter.readColumnNames(enduro.getResourcePath("/utils/testEmpty.csv"));
+		} catch (IllegalStateException e) {
+			success = true;
+		}
+		assertEquals("Should raise illegalStateException", true, success);
 	}
 }
