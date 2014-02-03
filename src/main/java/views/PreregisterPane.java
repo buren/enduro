24e2579e5e16fadcf2e43a7f21ea37ca7bf8a939
Oskar Holmberg <dat12oho@alfa-1.student.lth.se>
2014-02-03 13:42:52 +0100

    added preregister option

---
+++ src/main/java/views/PreregisterPane.java
@@ -0,0 +1,14 @@
+package views;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class PreregisterPane extends JOptionPane {
+
+	public PreregisterPane(String s) {
+		super(s);
+	}
+
+}
